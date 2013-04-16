24413ece935b4a7badde5012c39753257d318be4
Kristian Blom <kb@krbl.no>
2013-04-16 13:16:39 +0200

    Initial commit message

--- web/index_cluster.php
+++ web/index_cluster.php
@@ -5,7 +5,7 @@
  * @copyright Copyright (C) 1999-2013 eZ Systems AS. All rights reserved.
  * @license http://ez.no/eZPublish/Licenses/eZ-Trial-and-Test-License-Agreement-eZ-TTL-v2.0 eZ Trial and Test License Agreement Version 2.0
  */
-$legacyRoot = '/Users/kb/Development/keyteq-ez5-skeleton/ezpublish/../ezpublish_legacy';
+$legacyRoot = '/Users/kb/Development/ez5-demo/ezpublish/../ezpublish_legacy';
 chdir( $legacyRoot );
 require $legacyRoot . '/index_cluster.php';
 
